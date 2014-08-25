3fb9040259e8623fd9e5cd5f69fd9f8aaac3756e
Jay R Bolton <jayrbolton@gmail.com>
2014-08-25 15:25:22 -0700

    write stylings

--- js/app.js
+++ js/app.js
@@ -4,7 +4,14 @@ $(document).foundation();
 
 var $details_el = $('#address_detail');
 
-var substringMatcher = function(data) {
+function renderDetails(data) {
+  $details_el.hide().fadeIn()
+  $details_el.find('.addr').text(data.addr)
+  $details_el.find('.notified').text(data.notified)
+  $details_el.find('.status').text(data.status)
+}
+
+function substringMatcher(data) {
   return function(q, cb) {
     // regex used to determine if a string contains the substring `q`
     var regex = new RegExp(q, 'i');
@@ -13,7 +20,7 @@ var substringMatcher = function(data) {
       .slice(0,5); // return only first 5 results
     return cb(matches);
   };
-};
+}
 
 $.get('data/all.json', initTypeahead);
 
@@ -24,9 +31,18 @@ function initTypeahead(json) {
     source: substringMatcher(json.rows)
   })
   .on('typeahead:selected', function($e, sugg) {
-    $details_el.hide().fadeIn()
-    $details_el.find('.addr').text(sugg[0])
-    $details_el.find('.notified').text(sugg[1])
-    $details_el.find('.status').text(sugg[2])
+    renderDetails({addr: sugg[0], notified: sugg[1], status: sugg[2]})
   })
-};
+}
+
+function initMap() { 
+  var lat = 37.8043637
+  var lng = -122.2711137
+  var map = new google.maps.Map(document.getElementById("gmap_canvas"), {
+    zoom: 12,
+    center: new google.maps.LatLng(lat, lng),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  })
+}
+
+initMap()
